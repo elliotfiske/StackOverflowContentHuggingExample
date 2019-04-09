6bbc735ce30da0b8083531a06b797d9f5f7eef58
Elliot Fiske <elliot.fiske@salesforce.com>
2019-04-08 21:07:04 -0700

    Finished example

--- StackOverflowConstraintProblem/ViewController.swift
+++ StackOverflowConstraintProblem/ViewController.swift
@@ -10,10 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   @IBOutlet weak var testLabel: UILabel!
+   
+   override var keyCommands: [UIKeyCommand]? {
+      return [
+         UIKeyCommand(input: UIKeyCommand.inputUpArrow,
+                      modifierFlags: [],
+                      action: #selector(ViewController.addCharacter(sender:))),
+         UIKeyCommand(input: UIKeyCommand.inputDownArrow,
+                      modifierFlags: [],
+                      action: #selector(ViewController.rmCharacter(sender:)))
+      ]
+   }
+   
+   var numLetters = 1
+   
+   @objc func addCharacter(sender: UIKeyCommand) {
+      numLetters += 1
+      testLabel.text = String(format: "l%@bel", String(repeating: "a", count: numLetters))
+   }
+   
+   @objc func rmCharacter(sender: UIKeyCommand) {
+      numLetters -= 1
+      testLabel.text = String(format: "l%@bel", String(repeating: "a", count: numLetters))
+   }
+   
    override func viewDidLoad() {
       super.viewDidLoad()
       // Do any additional setup after loading the view, typically from a nib.
    }
+   
+   
 
 
 }
